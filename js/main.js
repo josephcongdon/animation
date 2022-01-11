0d546c54b2d0a25ea7724b175a1192eebd201ab1
Joseph Congdon <jocongdon@yahoo.com>
2022-01-11 01:38:28 -0600

    started the vocabulary for project

--- js/main.js
+++ js/main.js
@@ -3,3 +3,147 @@ $("#click").click(()=> {
    $(".testing").toggleClass("blue");
    $(".testing").html("<h3>This is blue</h3>");
 });
+
+//HTML
+// Block-Level Element
+// Inline Element
+// Void Element
+// Attributes
+// Nesting Element
+// List
+// Anchor
+// Image
+// Forms
+// Method
+// Action
+// Query String
+// Submitting
+// Inputs
+// Text Inputs
+// Text Areas
+// Checkboxes and Radio Buttons
+// Selects
+// Hidden Inputs
+// Name and Value
+// Disabled
+// Labels
+// CSS
+// Element Style Attribute
+// Inline/Embedded Stylesheet
+// External Stylesheet Comments
+// Basic Selectors
+// Multiple Selectors
+// Descendent Selector
+// Direct-Child Selector
+// Specificity
+// Inheriting Styles
+// Box Model
+// Floating Elements
+// Relative Positioning
+// Absolute Positioning
+// Fixed Positioning
+//
+// JavaScript
+// JavaScript
+// Boolean
+// Strings
+// Operator
+// Logical
+// Boolean
+// Comparison
+// Variable
+// Constant
+// Use Strict
+// Function
+// Argument
+// Parameter
+// Function Scope
+// Global Variables
+// Local Variables
+// Immediately-Invoked Function Expression (IIFE)
+// Conditionals
+// Unit Test
+// Loops
+// Arrays
+// Objects
+// Browser Object Model
+// Document Object Model
+// jQuery
+// Ajax Request
+// JavaScript Object Notation (JSON)
+// Promises
+// RESTful API Requests
+//
+// Java
+// Java
+// Components of Java
+// Statement
+// Data Types
+// Casting
+// Methods
+// Method Overloading
+// Recursion
+// Class
+// Field
+// Object
+// Instance Field
+// Static Field
+// Constructor
+// Visibility
+// Inheritance
+// Polymorphism
+// Overriding
+// Final Keyword
+// Abstract Class
+// Abstract Method
+// Interface
+// Collection
+// ArrayList
+// HashMap
+// Annotations
+// Exceptions
+// File
+// Path
+// JAR File
+// Dependencies
+// Maven
+// JUnit
+// Servlets
+// Tomcat
+// JSP
+// JSTL
+// MVC
+// Data Access Object (DAO)
+// Cookies
+// Session
+// JDBC
+// Hashing
+// Encryption
+//
+// MySql
+// MySQL
+// Query
+// Database
+// Schema
+// Primary Keys
+// Foreign Keys
+// Join
+// Inner Join
+// Left Join
+// Right Join
+// Subqueries
+// One-to-Many
+// Many-to-Many
+//
+// Spring
+// Controllers
+// Views
+// Repository
+// JPA
+// Dependency Injection
+// Relationships
+// Form Model Binding
+// Services
+// Security
+// Integration Tests
+// CRUD
---
+++ js/vocab.js
@@ -0,0 +1,270 @@
+const vocabulary = [
+
+    { term:'Block-Level Element', definition:'', language:'' },
+    { term:'Inline Element', definition:'', language:'' },
+    { term:'Void Element', definition:'', language:'' },
+    { term:'Attributes', definition:'', language:'' },
+    { term:'Nesting Element', definition:'', language:'' },
+    { term:'List', definition:'', language:'' },
+    { term:'Anchor', definition:'', language:'' },
+    { term:'Image', definition:'', language:'' },
+    { term:'Forms', definition:'', language:'' },
+    { term:'Method', definition:'', language:'' },
+    { term:'Action', definition:'', language:'' },
+    { term:'Query String', definition:'', language:'' },
+    { term:'Submitting', definition:'', language:'' },
+    { term:'Inputs', definition:'', language:'' },
+    { term:'Text Inputs', definition:'', language:'' },
+    { term:'Text Areas', definition:'', language:'' },
+    { term:'Checkboxes and Radio Buttons', definition:'', language:'' },
+    { term:'Selects', definition:'', language:'' },
+    { term:'Hidden Inputs', definition:'', language:'' },
+    { term:'Name and Value', definition:'', language:'' },
+    { term:'Disabled', definition:'', language:'' },
+    { term:'Labels', definition:'', language:'' },
+    { term:'CSS', definition:'', language:'' },
+    { term:'Element Style Attribute', definition:'', language:'' },
+    { term:'Inline/Embedded Stylesheet', definition:'', language:'' },
+    { term:'External Stylesheet Comments', definition:'', language:'' },
+    { term:'Basic Selectors', definition:'', language:'' },
+    { term:'Multiple Selectors', definition:'', language:'' },
+    { term:'Descendent Selector', definition:'', language:'' },
+    { term:'Direct-Child Selector', definition:'', language:'' },
+    { term:'Specificity', definition:'', language:'' },
+    { term:'Inheriting Styles', definition:'', language:'' },
+    { term:'Box Model', definition:'', language:'' },
+    { term:'Floating Elements', definition:'', language:'' },
+    { term:'Relative Positioning', definition:'', language:'' },
+    { term:'Absolute Positioning', definition:'', language:'' },
+    { term:'Fixed Positioning', definition:'', language:'' },
+    { term:'JavaScript', definition:'', language:'' },
+    { term:'Boolean', definition:'', language:'' },
+    { term:'Strings', definition:'', language:'' },
+    { term:'Operator', definition:'', language:'' },
+    { term:'Logical', definition:'', language:'' },
+    { term:'Boolean', definition:'', language:'' },
+    { term:'Comparison', definition:'', language:'' },
+    { term:'Variable', definition:'', language:'' },
+    { term:'Constant', definition:'', language:'' },
+    { term:'Use Strict', definition:'', language:'' },
+    { term:'Function', definition:'', language:'' },
+    { term:'Argument', definition:'', language:'' },
+    { term:'Parameter', definition:'', language:'' },
+    { term:'Function Scope', definition:'', language:'' },
+    { term:'Global Variables', definition:'', language:'' },
+    { term:'Local Variables', definition:'', language:'' },
+    { term:'Immediately-Invoked Function Expression (IIFE)', definition:'', language:'' },
+    { term:'Conditionals', definition:'', language:'' },
+    { term:'Unit Test', definition:'', language:'' },
+    { term:'Loops', definition:'', language:'' },
+    { term:'Arrays', definition:'', language:'' },
+    { term:'Objects', definition:'', language:'' },
+    { term:'Browser Object Model', definition:'', language:'' },
+    { term:'Document Object Model', definition:'', language:'' },
+    { term:'jQuery', definition:'', language:'' },
+    { term:'Ajax Request', definition:'', language:'' },
+    { term:'JavaScript Object Notation (JSON)', definition:'', language:'' },
+    { term:'Promises', definition:'', language:'' },
+    { term:'RESTful API Requests', definition:'', language:'' },
+    { term:'Java', definition:'', language:'' },
+    { term:'Components of Java', definition:'', language:'' },
+    { term:'Statement', definition:'', language:'' },
+    { term:'Data Types', definition:'', language:'' },
+    { term:'Casting', definition:'', language:'' },
+    { term:'Methods', definition:'', language:'' },
+    { term:'Method Overloading', definition:'', language:'' },
+    { term:'Recursion', definition:'', language:'' },
+    { term:'Class', definition:'', language:'' },
+    { term:'Field', definition:'', language:'' },
+    { term:'Object', definition:'', language:'' },
+    { term:'Instance Field', definition:'', language:'' },
+    { term:'Static Field', definition:'', language:'' },
+    { term:'Constructor', definition:'', language:'' },
+    { term:'Visibility', definition:'', language:'' },
+    { term:'Inheritance', definition:'', language:'' },
+    { term:'Polymorphism', definition:'', language:'' },
+    { term:'Overriding', definition:'', language:'' },
+    { term:'Final Keyword', definition:'', language:'' },
+    { term:'Abstract Class', definition:'', language:'' },
+    { term:'Abstract Method', definition:'', language:'' },
+    { term:'Interface', definition:'', language:'' },
+    { term:'Collection', definition:'', language:'' },
+    { term:'ArrayList', definition:'', language:'' },
+    { term:'HashMap', definition:'', language:'' },
+    { term:'Annotations', definition:'', language:'' },
+    { term:'Exceptions', definition:'', language:'' },
+    { term:'File', definition:'', language:'' },
+    { term:'Path', definition:'', language:'' },
+    { term:'JAR File', definition:'', language:'' },
+    { term:'Dependencies', definition:'', language:'' },
+    { term:'Maven', definition:'', language:'' },
+    { term:'JUnit', definition:'', language:'' },
+    { term:'Servlets', definition:'', language:'' },
+    { term:'Tomcat', definition:'', language:'' },
+    { term:'JSP', definition:'', language:'' },
+    { term:'JSTL', definition:'', language:'' },
+    { term:'MVC', definition:'', language:'' },
+    { term:'Data Access Object (DAO)', definition:'', language:'' },
+    { term:'Cookies', definition:'', language:'' },
+    { term:'Session', definition:'', language:'' },
+    { term:'JDBC', definition:'', language:'' },
+    { term:'Hashing', definition:'', language:'' },
+    { term:'Encryption', definition:'', language:'' },
+    { term:'MySQL', definition:'', language:'' },
+    { term:'Query', definition:'', language:'' },
+    { term:'Database', definition:'', language:'' },
+    { term:'Schema', definition:'', language:'' },
+    { term:'Primary Keys', definition:'', language:'' },
+    { term:'Foreign Keys', definition:'', language:'' },
+    { term:'Join', definition:'', language:'' },
+    { term:'Inner Join', definition:'', language:'' },
+    { term:'Left Join', definition:'', language:'' },
+    { term:'Right Join', definition:'', language:'' },
+    { term:'Subqueries', definition:'', language:'' },
+    { term:'One-to-Many', definition:'', language:'' },
+    { term:'Many-to-Many', definition:'', language:'' },
+    { term:'Spring', definition:'', language:'' },
+    { term:'Controllers', definition:'', language:'' },
+    { term:'Views', definition:'', language:'' },
+    { term:'Repository', definition:'', language:'' },
+    { term:'JPA', definition:'', language:'' },
+    { term:'Dependency Injection', definition:'', language:'' },
+    { term:'Relationships', definition:'', language:'' },
+    { term:'Form Model Binding', definition:'', language:'' },
+    { term:'Services', definition:'', language:'' },
+    { term:'Security', definition:'', language:'' },
+    { term:'Integration Tests', definition:'', language:'' },
+    { term:'CRUD', definition:'', language:'' },
+];
+
+// HTML
+// Block-Level Element
+// Inline Element
+// Void Element
+// Attributes
+// Nesting Element
+// List
+// Anchor
+// Image
+// Forms
+// Method
+// Action
+// Query String
+// Submitting
+// Inputs
+// Text Inputs
+// Text Areas
+// Checkboxes and Radio Buttons Selects
+// Hidden Inputs
+// Name and Value
+// Disabled
+// Labels
+// CSS
+// Element Style Attribute Inline/Embedded Stylesheet External Stylesheet Comments
+// Basic Selectors
+// Multiple Selectors
+// Descendent Selector Direct-Child Selector Specificity
+// Inheriting Styles
+// Box Model
+// 2
+// Floating Elements
+// Relative Positioning
+// Absolute Positioning
+// Fixed Positioning
+//
+// JavaScript
+// JavaScript
+// Boolean
+// Strings
+// Operator
+// Logical
+// Boolean
+// Comparison
+// Variable
+// Constant
+// Use Strict
+// Function
+// Argument
+// Parameter
+// Function Scope
+// Global Variables
+// Local Variables
+// Immediately-Invoked Function Expression (IIFE) Conditionals
+// Unit Test
+// Loops
+// Arrays
+// Objects
+// Browser Object Model Document Object Model jQuery
+// Ajax Request
+// JavaScript Object Notation (JSON) Promises
+// RESTful API Requests
+//
+// Java
+// Java
+// Components of Java
+// Statement
+// Data Types
+// Casting
+// Methods
+// Method Overloading
+// Recursion
+// Class
+// Field
+// Object
+// Instance Field
+// Static Field
+// Constructor
+// Visibility
+// Inheritance
+// Polymorphism
+// Overriding
+// Final Keyword
+// Abstract Class
+// Abstract Method
+// Interface
+// Collection
+// ArrayList
+// HashMap
+// Annotations
+// Exceptions
+// File
+// Path
+// JAR File
+// Dependencies
+// Maven
+// JUnit
+// Servlets
+// Tomcat
+// JSP
+// JSTL
+// MVC
+// Data Access Object (DAO) Cookies
+// Session JDBC Hashing Encryption
+//
+// MySql
+// MySQL
+// Query
+// Database
+// Schema
+// Primary Keys
+// Foreign Keys
+// Join
+// Inner Join
+// Left Join
+// Right Join
+// Subqueries
+// One-to-Many
+// Many-to-Many
+//
+// Spring
+// Controllers
+// Views
+// Repository
+// JPA
+// Dependency Injection
+// Relationships
+// Form Model Binding
+// Services
+// Security
+// Integration Tests
+// CRUD
