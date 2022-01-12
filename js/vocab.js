fa1d3794b1901009b11a881e75fc80a2ad9007f8
Joseph Congdon <jocongdon@yahoo.com>
2022-01-11 23:27:21 -0600

    got the terms to work

--- js/vocab.js
+++ js/vocab.js
@@ -1,5 +1,5 @@
 const vocabulary = [
-
+    { term:'HTML', definition:'', language:'' },
     { term:'Block-Level Element', definition:'', language:'' },
     { term:'Inline Element', definition:'', language:'' },
     { term:'Void Element', definition:'', language:'' },
@@ -137,134 +137,12 @@ const vocabulary = [
     { term:'CRUD', definition:'', language:'' },
 ];
 
-// HTML
-// Block-Level Element
-// Inline Element
-// Void Element
-// Attributes
-// Nesting Element
-// List
-// Anchor
-// Image
-// Forms
-// Method
-// Action
-// Query String
-// Submitting
-// Inputs
-// Text Inputs
-// Text Areas
-// Checkboxes and Radio Buttons Selects
-// Hidden Inputs
-// Name and Value
-// Disabled
-// Labels
-// CSS
-// Element Style Attribute Inline/Embedded Stylesheet External Stylesheet Comments
-// Basic Selectors
-// Multiple Selectors
-// Descendent Selector Direct-Child Selector Specificity
-// Inheriting Styles
-// Box Model
-// 2
-// Floating Elements
-// Relative Positioning
-// Absolute Positioning
-// Fixed Positioning
-//
-// JavaScript
-// JavaScript
-// Boolean
-// Strings
-// Operator
-// Logical
-// Boolean
-// Comparison
-// Variable
-// Constant
-// Use Strict
-// Function
-// Argument
-// Parameter
-// Function Scope
-// Global Variables
-// Local Variables
-// Immediately-Invoked Function Expression (IIFE) Conditionals
-// Unit Test
-// Loops
-// Arrays
-// Objects
-// Browser Object Model Document Object Model jQuery
-// Ajax Request
-// JavaScript Object Notation (JSON) Promises
-// RESTful API Requests
-//
-// Java
-// Java
-// Components of Java
-// Statement
-// Data Types
-// Casting
-// Methods
-// Method Overloading
-// Recursion
-// Class
-// Field
-// Object
-// Instance Field
-// Static Field
-// Constructor
-// Visibility
-// Inheritance
-// Polymorphism
-// Overriding
-// Final Keyword
-// Abstract Class
-// Abstract Method
-// Interface
-// Collection
-// ArrayList
-// HashMap
-// Annotations
-// Exceptions
-// File
-// Path
-// JAR File
-// Dependencies
-// Maven
-// JUnit
-// Servlets
-// Tomcat
-// JSP
-// JSTL
-// MVC
-// Data Access Object (DAO) Cookies
-// Session JDBC Hashing Encryption
-//
-// MySql
-// MySQL
-// Query
-// Database
-// Schema
-// Primary Keys
-// Foreign Keys
-// Join
-// Inner Join
-// Left Join
-// Right Join
-// Subqueries
-// One-to-Many
-// Many-to-Many
-//
-// Spring
-// Controllers
-// Views
-// Repository
-// JPA
-// Dependency Injection
-// Relationships
-// Form Model Binding
-// Services
-// Security
-// Integration Tests
-// CRUD
+function showVocabulary(){
+        let html = "";
+    vocabulary.forEach((word) => {
+        html += '<h3>'+ word.term + '</h3>'
+    })
+    $('#term').append(html);
+}
+
+showVocabulary(vocabulary)
